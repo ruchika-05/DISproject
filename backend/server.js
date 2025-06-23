f98d289a855ab40918ec334128ac273f0533d77e
Ruchika Yadav <ruchika.yadav1285@gmail.com>
2025-06-23 15:48:30 +0530

    some deployment changes

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
@@ -25,6 +25,9 @@ db.connect(err => {
     }
 });
 
+app.get("/", (req, res) => {
+  res.send("Backend is live!");
+});
 // USER REGISTRATION
 app.post("/api/register", (req, res) => {
     const { name, email, phone, password, address, pincode, state } = req.body;
