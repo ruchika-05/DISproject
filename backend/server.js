5cb0e356c2dc97c8980ba8582c42ead3391a1828
Ruchika Yadav <ruchika.yadav1285@gmail.com>
2025-06-24 13:20:02 +0530

    p

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD, // Now securely loaded from .env
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
 });
 
 db.connect(err => {
