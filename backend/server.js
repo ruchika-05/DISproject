27df2e9e05e0990bb2f0c74e19eee6c273de3a66
Ruchika Yadav <ruchika.yadav1285@gmail.com>
2025-06-24 13:02:53 +0530

    ..

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,12 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 5000;
-
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD, // Now securely loaded from .env
     database: process.env.DB_NAME,
+    port:process.env.PORT
 });
 
 db.connect(err => {
